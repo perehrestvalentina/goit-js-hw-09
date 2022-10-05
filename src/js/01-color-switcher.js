3d20b8c4e0631134517ad2078493e3c6a6cb56d6
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-10-06 00:32:05 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+const INTERVAL_DELAY = 1000;
+btnStart.disabled = false;
+btnStop.disabled = true;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function updateBackgroundColor (){
+    body.style.backgroundColor =  getRandomHexColor();
+}  
+
+btnStart.addEventListener('click', onBtnStartClick);
+
+function onBtnStartClick(){
+    console.log(`Call onBtnStartClick every ${INTERVAL_DELAY} ms`)
+
+    timerId = setInterval(updateBackgroundColor, INTERVAL_DELAY);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+};
+
+btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStopClick(){
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(timerId);
+    console.log(`Interval has stopped!`);
+};
