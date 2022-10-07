91c6f86d71f6fe9de55bcd20acc9fc358c25b351
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-10-08 00:49:49 +0300

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,29 +11,28 @@ function onFormSubmit(e){
   let amount = Number(e.currentTarget.amount.value);
 
 for (let position = 1; position <= amount; position += 1) {
-  createPromise(position, delay)
-  .then(({ position, delay }) => {
-    setTimeout(() => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    }, delay)
-  })
-  .catch(({ position, delay }) => { 
-    setTimeout(() => {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    }, delay) 
-  });
+  createPromise(position, delay);
+ 
 delay += step;
-}
+};
 }
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  let promiseValue = {position, delay};
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-  return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve(promiseValue);
-    }
-      reject(promiseValue);
-  })
-};
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise.then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  }).catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+}
